9e95fb4add69cf4fe79a7328f74994b6cd6b5c65
Omar-hamdy <Omarzizo27@gmail.com>
2022-09-03 20:15:42 +0200

    this is new program

---
+++ Untitled1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+int main(){
+
+int x;
+ int i;
+  int sum, num;
+ printf("Enter Integer number:");
+  scanf("%d",&x);
+  //sum = x;
+
+for ( i=0 ; x > 1 ; i++){
+  printf("i : %d\n",i);
+    x = x/10;
+}
+
+printf("the digits number is :%d",i);
+ return 0;
+}
